8987c763e176fedf427a515c45f0c5efb766dbb0
Shiro <ducduydinh4805nt@gmail.com>
2026-06-03 18:34:41 +0700

    fix: use standard compile sdk declaration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,7 @@ plugins {
 
 android {
     namespace = "com.project.minlishapp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.project.minlishapp"
